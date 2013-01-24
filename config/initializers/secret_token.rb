2c81edd84b0b500a42fa8b173aae6242e54ebdf3
Michael Gerow <gerow@usc.edu>
2013-01-24 00:11:00 -0800

    Initial commit of rogo.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rogo::Application.config.secret_token = 'f31fb51725d12d2d3e4bc549a5e63552ecfef7051a510359244b1ea6e1cc90d252bfe4edd6d1771d92923d27915c4d4b3eed3c5fdabec0e44ad25275f149a56b'
