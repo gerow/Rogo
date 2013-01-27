e715d3ee0465efd120604f0913f6863f461aa977
Michael Gerow <gerow@usc.edu>
2013-01-26 22:53:36 -0800

    Turned that bit of animation code into a function.  Neato.

--- app/assets/javascripts/rogomes/index.js
+++ app/assets/javascripts/rogomes/index.js
@@ -1,11 +1,15 @@
+fade_new_html = function(field, new_html) {
+  field.animate({opacity: 0}, 500, function() {
+    field.html(new_html);
+    field.animate({opacity: 1},500);
+  });
+}
+
 $('document').ready(function() {
   if ($('body.rogomes').length) {
     $('div.creator_answer').click(function(){
       $(this).unbind('click')
-      $(this).animate({opacity: 0}, 500, function() {
-        $(this).html("BLAHAHA");
-        $(this).animate({opacity: 1},500);
-      });
+      fade_new_html($(this), "WHATEVA")
     });
   }
 });
