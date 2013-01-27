cf30cc3c1b540830808a162263286a9e9b9347ea
Michael Gerow <gerow@usc.edu>
2013-01-26 23:44:46 -0800

    Added question IDs to all the answer forms so that we can post to the right url

--- app/assets/javascripts/rogomes/index.js
+++ app/assets/javascripts/rogomes/index.js
@@ -9,7 +9,7 @@ $('document').ready(function() {
   if ($('body.rogomes').length) {
     $('div.creator_answer').click(function(){
       $(this).unbind('click')
-      fade_new_html($(this), "WHATEVA")
+      fade_new_html($(this), "WHATEVA</br></br>WHATEVAELSE")
     });
   }
 });
