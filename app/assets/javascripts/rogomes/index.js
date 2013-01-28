c450a3414b948a874d510d5d257b8526a7dac33e
gerow <gerow.mike@gmail.com>
2013-01-27 21:56:28 -0800

    Taking a short break from working on comments

--- app/assets/javascripts/rogomes/index.js
+++ app/assets/javascripts/rogomes/index.js
@@ -5,11 +5,13 @@ fade_new_html = function(field, new_html) {
   });
 }
 
+var question_response_form = "<h1>%%QID%%</h1>" 
+
 $('document').ready(function() {
   if ($('body.rogomes').length) {
     $('div.creator_answer').click(function(){
       $(this).unbind('click')
-      fade_new_html($(this), "WHATEVA</br></br>WHATEVAELSE")
+      fade_new_html($(this), question_response_form.replace("%%QID%%", $(this).attr('id')))
     });
   }
 });
