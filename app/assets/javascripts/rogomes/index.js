e800b188101439d092b2a925e1339ee60d037fb2
gerow <gerow.mike@gmail.com>
2013-01-29 18:01:37 -0800

    Answering a question now actually brings up a form to do so

--- app/assets/javascripts/rogomes/index.js
+++ app/assets/javascripts/rogomes/index.js
@@ -5,7 +5,7 @@ fade_new_html = function(field, new_html) {
   });
 }
 
-var question_response_form = "<h1>%%QID%%</h1>" 
+var question_response_form = '<form action="/questions/%%QID%%/answer" method="post"><input type="text" name="answer"><input type="submit" value="Submit"></form>' 
 
 $('document').ready(function() {
   if ($('body.rogomes').length) {
